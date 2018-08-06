fd16b2c0d4243f9da515bb809dc33d34a77f66f8
Thesand <grusetsomlever@gmail.com>
2018-08-06 23:04:34 +0200

    'Working' 3rd person camera
    Just made an working 3rd person camera prototype.

---
+++ LD_WarmUp/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+    public bool setting_isPaused;
+
+	// Use this for initialization
+	void Start () {
+        SetIsPaused(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetIsPaused(bool set)
+    {
+        this.setting_isPaused = set;
+        if (set)
+            Cursor.lockState = CursorLockMode.None;
+        else
+            Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = set;
+    }
+}
