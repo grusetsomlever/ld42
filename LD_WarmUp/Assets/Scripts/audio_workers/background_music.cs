95fb20c816131f6477434fb07a3151a2d8ea5e37
mjgood <mjgood91@gmail.com>
2018-08-09 23:06:41 -0400

    Revert "Revert "Added an Audio file""
    This reverts commit 56a1facb7fb4b2c50503b21ef49b63d1311df8e3.

---
+++ LD_WarmUp/Assets/Scripts/audio_workers/background_music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_music : MonoBehaviour {
+
+    public AudioSource startMusic;
+    public AudioSource loopMusic;
+    private bool loopMusicStarted = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!startMusic.isPlaying && loopMusicStarted == false) {
+            loopMusic.Play();
+            loopMusicStarted = true;
+        }
+    }
+}
